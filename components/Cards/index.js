b54eb4e2e3a9e22f683a2e5a7749894a9855ad15
Adella <scratchglory@gmail.com>
2019-12-13 19:28:44 -0500

    up to 3rd step

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const cardCont = document.querySelector(".cards-container");
+function cardCreator(res) {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgCont = document.createElement("div");
+  const span = document.createElement("span");
+  const img = document.createElement("img");
+
+  // html
+  cardCont.appendChild(card);
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgCont);
+  author.appendChild(span);
+  imgCont.appendChild(img);
+
+  // css
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgCont.classList.add("img-container");
+
+  // text-src
+  // img.src = res
+
+  return card;
+}
+
+console.log(cardCreator());
